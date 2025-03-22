72a038d35b972aa06c31e6c708920762f5a016fd
Miguel <joao.miguel@inbraep.com.br>
2025-03-22 11:51:38 -0300

    Inicio do front

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Bem vindo ao mangaWave</h1>
+    <form class="form">
+        <input 
+            type="text"
+            v-model="form.name"
+            placeholder="O que você está procurando?">
+            <button> <i class="fa-brands fa-searchengin"></i>aaa</button>
+    </form>
+    <div class="links">
+        <a href="#">Créditos</a>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      form: {
+        name: ""
+      } 
+    }
+  }
+}
+</script>
